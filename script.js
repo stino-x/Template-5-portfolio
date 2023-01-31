6aedf47f41d1dc6cc27fd6724939bc3d29ed4f0d
francksefu <90141102+francksefu@users.noreply.github.com>
2023-01-31 20:26:34 +0200

    Add the hidden and show feature in script.js file

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const cancel = document.queryselector('.cancel');
-const hamburger = document.querySelector('.hamburger');
-const navmenu = document.querySelector('.nav-menu');
-const menu = document.querySelectorAll('h-menu');
-function display () {
-  if()
+const menu = document.querySelector('#menu-whole');
+const x = document.querySelector('#x');
+const smallMen = document.querySelectorAll('.h-menu');
+const main = document.querySelector('main');
+const hamber = document.querySelector('#hamber');
+
+function display() {
+  if(menu.style.display === 'none') {
+    menu.style.display = 'block';
+    main.style.display = 'none';
+    hamber.style.display = 'none';
+  }else {
+    menu.style.display = 'none';
+    main.style.display = 'block';
+    hamber.style.display = 'block';
+  }
+}
+
+function hide() {
+  menu.style.display = 'none';
+  
+  hamber.style.display = 'block';
+  main.style.display = 'block';
+}
+
+hamber.addEventListener('click', display);
+x.addEventListener('click', display);
+
+for (let i = 0; i < smallMen.length; i += 1) {
+  smallMen[i].addEventListener('click', hide);
 }
