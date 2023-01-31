b954c345e27bb78e1896d1ec88c95472e1939c0a
stino-x <107426247+stino-x@users.noreply.github.com>
2023-01-31 18:40:39 +0100

    Added menu skeleton
    i implemented the html/css for the menu  i will make functional in the next commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const cancel = document.queryselector('.cancel');
+const hamburger = document.querySelector('.hamburger');
+const navmenu = document.querySelector('.nav-menu');
+const menu = document.querySelectorAll('h-menu');
+function display () {
+  if()
+}
