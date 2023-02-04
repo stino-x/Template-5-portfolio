194f9217212e99eaac02cbf91a06948a64b82a1e
stino-x <107426247+stino-x@users.noreply.github.com>
2023-02-04 01:28:29 +0100

    form-validation
    form-validation complete

--- form-validation.js
+++ form-validation.js
@@ -1,30 +1,45 @@
 const form = document.querySelector('#form-desk');
+const mobileForm = document.querySelector('#mobile-form');
 const email = document.querySelector('#email-desk');
+const mobileEmail = document.querySelector('#email-mobile');
 const message = document.querySelector('#messageEmail');
+const messageMobile = document.querySelector('#messageEmail-mobile');
 
 function validateDesk() {
   const compare = email.value.toLowerCase();
-  if (email.value == compare) {
+  if (email.value === compare) {
     email.style.borderColor = 'inherit';
     message.innerText = '';
     return true;
-  } else {
-    email.style.borderColor = 'red';
-    message.innerText = 'remove Uppercase character please';
-    message.style.color = 'red';
-    return false;
   }
+  email.style.borderColor = 'red';
+  message.innerText = 'remove ALL Uppercase characters please';
+  message.style.color = 'red';
+  return false;
 }
-
-/*const but = document.querySelector('#but');
-
-but.addEventListener('click', () => {
-  console.log(validateDesk());
-});*/
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (validateDesk()) {
     form.submit();
+  }
+});
+
+function validateMobile() {
+  const compare = mobileEmail.value.toLowerCase();
+  if (mobileEmail.value === compare) {
+    mobileEmail.style.borderColor = 'inherit';
+    messageMobile.innerText = '';
+    return true;
+  }
+  mobileEmail.style.borderColor = 'red';
+  messageMobile.innerText = 'remove ALL Uppercase characters please';
+  messageMobile.style.color = 'red';
+  return false;
+}
+
+mobileForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (validateMobile()) {
+    mobileForm.submit();
   }
 });
