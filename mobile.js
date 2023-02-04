f75d003f7002c8cb4729bb1d3882df8c666de878
francksefu <90141102+francksefu@users.noreply.github.com>
2023-02-04 18:13:23 +0200

    See what to do

---
+++ mobile.js
@@ -0,0 +1,19 @@
+const about = document.querySelector('#about');
+const mother = document.querySelector('main');
+const section = document.createElement('section');
+
+
+section.classList.add('section-2');
+section.setAttribute('id', 'portfolio');
+h2.innerText = bodyS.text;
+
+
+mother.insertBefore(section, about);
+
+const body = document.body;
+const bodyS = [{
+    css: `width: 98vw; height: 100vh;`,
+    text: 'franck'
+},{ css1: 'width: 100%, height: 300px'}]
+
+body.style.cssText = bodyS[0].css; 
