f6cb58d8e169d8afa7850257364b98c3685b67a2
stino-x <107426247+stino-x@users.noreply.github.com>
2023-02-11 22:59:24 +0100

    fixing styling issues

--- form-validation.js
+++ form-validation.js
@@ -7,25 +7,32 @@ const messageMobile = document.querySelector('#messageEmail-mobile');
 const nameInput = document.querySelector('.name');
 const emailInput = document.querySelector('.email');
 const messageInput = document.querySelector('.message');
+// const rawData = localStorage.getItem('cachedFormData');
+
 // const formbutton = document.querySelector('#form-button');
 // const formbuttondesktop = document.querySelector('#form-button-desk');
 
-const formobject = {
-  name: '',
-  email: '',
-  message: '',
-};
+// const formobject = {
+//   name: '',
+//   email: '',
+//   message: '',
+// };
 
-function storeInput() {
-  const stringobject = JSON.stringify(formobject);
-  nameInput.value = formobject.name;
-  emailInput.value = formobject.email;
-  messageInput.value = formobject.message;
-  localStorage.setItem('formobject', stringobject);
-}
+// if (rawData) {
+//   const formobject = JSON.parse(rawData);
+//   emailInput.value = formobject.email;
+//   nameInput.value = formobject.name;
+//   messageInput.value = formobject.message;
+// }
 
-form.addEventListener('click', storeInput());
-mobileForm.addEventListener('click', storeInput());
+// const dataStore = (event) => {
+//   const element = event.target;
+//   formobject[element.name] = element.value;
+//   localStorage.setItem('cachedFormData', JSON.stringify(formobject));
+// };
+// nameInput.addEventListener('change', dataStore);
+// emailInput.addEventListener('change', dataStore);
+// messageInput.addEventListener('change', dataStore);
 
 function validateDesk() {
   const compare = email.value.toLowerCase();
