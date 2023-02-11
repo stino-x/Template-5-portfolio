49eadc900ee7c079d27c8521da011e0bd178b6ca
Hamdullah-Andar <hamdullahandar786@gmail.com>
2023-02-12 02:32:39 +0430

    Add local Storage

--- form-validation.js
+++ form-validation.js
@@ -7,32 +7,32 @@ const messageMobile = document.querySelector('#messageEmail-mobile');
 const nameInput = document.querySelector('.name');
 const emailInput = document.querySelector('.email');
 const messageInput = document.querySelector('.message');
-// const rawData = localStorage.getItem('cachedFormData');
+const rawData = localStorage.getItem('cachedFormData');
 
 // const formbutton = document.querySelector('#form-button');
 // const formbuttondesktop = document.querySelector('#form-button-desk');
 
-// const formobject = {
-//   name: '',
-//   email: '',
-//   message: '',
-// };
+const formobject = {
+  name: '',
+  email: '',
+  message: '',
+};
 
-// if (rawData) {
-//   const formobject = JSON.parse(rawData);
-//   emailInput.value = formobject.email;
-//   nameInput.value = formobject.name;
-//   messageInput.value = formobject.message;
-// }
+if (rawData) {
+  const formobject = JSON.parse(rawData);
+  emailInput.value = formobject.email;
+  nameInput.value = formobject.name;
+  messageInput.value = formobject.message;
+}
 
-// const dataStore = (event) => {
-//   const element = event.target;
-//   formobject[element.name] = element.value;
-//   localStorage.setItem('cachedFormData', JSON.stringify(formobject));
-// };
-// nameInput.addEventListener('change', dataStore);
-// emailInput.addEventListener('change', dataStore);
-// messageInput.addEventListener('change', dataStore);
+const dataStore = (event) => {
+  const element = event.target;
+  formobject[element.name] = element.value;
+  localStorage.setItem('cachedFormData', JSON.stringify(formobject));
+};
+nameInput.addEventListener('change', dataStore);
+emailInput.addEventListener('change', dataStore);
+messageInput.addEventListener('change', dataStore);
 
 function validateDesk() {
   const compare = email.value.toLowerCase();
