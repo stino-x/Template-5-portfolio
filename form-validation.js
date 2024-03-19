77b618b8860fed26b39cbaf5bcbde4dbeec44499
stino-x <107426247+stino-x@users.noreply.github.com>
2024-03-19 15:52:53 +0100

    build(deps): update package-lock.json, package.json and styles.css

---
+++ form-validation.js
@@ -0,0 +1,71 @@
+const form = document.querySelector('#form-desk');
+const mobileForm = document.querySelector('#mobile-form');
+const email = document.querySelector('#email-desk');
+const mobileEmail = document.querySelector('#email-mobile');
+const message = document.querySelector('#messageEmail');
+const messageMobile = document.querySelector('#messageEmail-mobile');
+const nameInput = document.querySelector('.name');
+const emailInput = document.querySelector('.email');
+const messageInput = document.querySelector('.message');
+const rawData = localStorage.getItem('cachedFormData');
+
+const formobject = {
+  name: '',
+  email: '',
+  message: '',
+};
+
+if (rawData) {
+  const formobject = JSON.parse(rawData);
+  emailInput.value = formobject.email;
+  nameInput.value = formobject.name;
+  messageInput.value = formobject.message;
+}
+
+const dataStore = (event) => {
+  const element = event.target;
+  formobject[element.name] = element.value;
+  localStorage.setItem('cachedFormData', JSON.stringify(formobject));
+};
+nameInput.addEventListener('change', dataStore);
+emailInput.addEventListener('change', dataStore);
+messageInput.addEventListener('change', dataStore);
+
+function validateDesk() {
+  const compare = email.value.toLowerCase();
+  if (email.value === compare) {
+    email.style.borderColor = 'inherit';
+    message.innerText = '';
+    return true;
+  }
+  email.style.borderColor = 'red';
+  message.innerText = 'Emails cannot take CAPITAL Letters submit valid email';
+  message.style.color = 'red';
+  return false;
+}
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (validateDesk()) {
+    form.submit();
+  }
+});
+
+function validateMobile() {
+  const compare = mobileEmail.value.toLowerCase();
+  if (mobileEmail.value === compare) {
+    mobileEmail.style.borderColor = 'inherit';
+    messageMobile.innerText = '';
+    return true;
+  }
+  mobileEmail.style.borderColor = 'red';
+  messageMobile.innerText = 'Emails cannot take CAPITAL Letters submit valid email';
+  messageMobile.style.color = 'red';
+  return false;
+}
+
+mobileForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (validateMobile()) {
+    mobileForm.submit();
+  }
+});
