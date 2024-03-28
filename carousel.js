74dc0fdc322d0a54cd27cb2d1d4459a18d5ae94f
stino-x <107426247+stino-x@users.noreply.github.com>
2024-03-28 17:06:03 +0100

    made carousel responsive

--- carousel.js
+++ carousel.js
@@ -1,17 +1,44 @@
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-  var swiper2 = new Swiper(".mySwiper2", {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
-  
+const leftBtn = document.querySelector('.btn-left');
+const rightBtn = document.querySelector('.btn-right');
+const imgContainer = document.querySelector('.img-container');
+const imgs = document.querySelectorAll('.img-grid img'); 
+
+const images = ['img-0', 'img-1', 'img-2', 'img-3', 'img-4'];
+let counter = 0;
+
+const blurImages = () => { 
+  imgs.forEach(img=> img.style.opacity = '0.3')
+}
+
+const moveLeft = () => { 
+  counter--; 
+  blurImages();
+  if (counter < 0) { 
+    counter = imgs.length - 1
+  }
+  imgContainer.style.backgroundImage = `url(./imgs/${images[counter]}.jpg)`;
+  imgs[counter].style.opacity = '1';
+}
+
+const moveRight = () => { 
+  counter++; 
+   blurImages();
+  if (counter >= images.length) { 
+    counter = 0;
+  }
+    imgContainer.style.backgroundImage = `url(./imgs/${images[counter]}.jpg)`;
+		imgs[counter].style.opacity = "1";
+}
+
+leftBtn.addEventListener('click', moveLeft)
+rightBtn.addEventListener('click', moveRight)
+
+imgs.forEach((img, index) => { 
+  img.addEventListener('click', (e) => { 
+    imgContainer.style.backgroundImage = `url(${img.getAttribute("src")})`
+    blurImages();
+    img.style.opacity = '1';
+    counter = index;
+  })
+})
+
